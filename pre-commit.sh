4b5de6b3eba83638ac737183ffed683b9d424d71
Gianni Tedesco <gianni@scaramanga.co.uk>
2022-10-15 19:19:16 +0900

    Only run mypy once

--- pre-commit.sh
+++ pre-commit.sh
@@ -7,16 +7,6 @@ exec 1>&2
 pkgname='minotaur'
 scripts=setup.py
 
-mypy --strict ${pkgname}
-mypy \
-	--disallow-any-generics \
-	--warn-redundant-casts \
-	--warn-unused-ignores \
-	--no-warn-no-return \
-	--warn-return-any \
-	--check-untyped-defs \
-	--no-implicit-optional \
-	--ignore-missing \
-	${scripts}
+mypy --strict ${pkgname} ${scripts}
 
 flake8 ${pkgname} ${scripts}
